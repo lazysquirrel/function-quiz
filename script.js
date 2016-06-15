083081fa08b45f50f7d1415ec7ff7a9454857052
Tine <tine@toll.de>
2016-06-15 09:51:53 +0200

    add latex visualisierung

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-var Questions =
-    [
-        {
-            answers: ["sin(x * y)", "cos(x+y)", "1/ln(x*y))", "2x+5y"],
-            alreadyAsked: false
-        },
-        {
-            answers: ["sqrt(x*y)", "cos(x+y)", "1/ln(x*y))", "2x+5y"],
-            alreadyAsked: false
-        },
-        {
-            answers: ["(x*x) +2.5*(y*y)-y)*exp(1-((x*x)+(y*y)))* 0.3", "3y", "3z*4y", "cos(x + y * 5)"],
-            alreadyAsked: false
-        },
-        {
-            answers: ["1 - (x*x+y*y)", "cos(x+y)", "1/ln(x*y))", "2x+5y"],
-            alreadyAsked: false
-        }
-    ];
-
 var currentQuiz;
 
 function showQuestion(random) {
@@ -47,6 +27,7 @@ function startQuiz() {
 
     if (!Questions[random].alreadyAsked) {
         showQuestion(random);
+		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
     }
 }
 
