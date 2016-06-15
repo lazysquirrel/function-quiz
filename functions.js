eddb44ef77b32dfe06b5e6d08cd0dd3b4ce68527
Tine <tine@toll.de>
2016-06-15 10:20:52 +0200

    add new functions

--- functions.js
+++ functions.js
@@ -2,28 +2,29 @@
 var Questions =
     [
         {
-            //answers: ["sin(x * y) \\( a \ne 0 \\)", "cos(x+y)", "1/ln(x*y))", "2x+5y"],
-			answers: ["` sin(x * y)) `", "` cos(x+y) `", "` 1/ln(x*y) `", "` 2x+5y `"],
-            // alreadyAsked: false
-        // },
-        // {
-            // //answers: ["sqrt(x*y) \\( a \ne 0 \\)", "cos(x+y)", "1/ln(x*y))", "2x+5y"],
-            // alreadyAsked: false
-        // },
-        // {
-            // //answers: ["(x*x) +2.5*(y*y)-y)*exp(1-((x*x)+(y*y)))* 0.3", "3y \\( a \ne 0 \\)", "3z*4y", "cos(x + y * 5)"],
-            // alreadyAsked: false
-        // },
-        // {
-            // //answers: ["1 - (x*x+y*y)", "cos(x+y) \\( a \ne 0 \\)", "1/ln(x*y))", "2x+5y"],
-            // alreadyAsked: false
-        // },
-        // {
-            // //answers: ["sin(x+y)*cos(y+x) \\( a \ne 0 \\)", "cos(x+y)*sin(x*y)", "sin(x*y)*cos(y*x)", "tan(x+y)"],
-            // alreadyAsked: false
-        // },
-        // {
-            // //answers: ["ln(1/x)*y \\( a \ne 0 \\)", "cos(x+y)*sin(x*y)", "sin(x*y)*cos(y*x)", "tan(x+y)"],
-            // alreadyAsked: false
+			answers: ["` sin(x * y) `", "` cos(x+y) `", "` 1/ln(x*y) `", "` 2x+5y `"],
+            alreadyAsked: false
+        },
+        {
+            //answers: ["sqrt(x*y) \\( a \ne 0 \\)", "cos(x+y)", "1/ln(x*y))", "2x+5y"],
+			answers: ["` sqrt(x*y) `", "` cos(x+y) `", "` 1/ln(x*y) `", "` 2x+5y `"],
+            alreadyAsked: false
+        },
+        {
+            //answers: ["(x*x) +2.5*(y*y)-y)*exp(1-((x*x)+(y*y)))* 0.3", "3y \\( a \ne 0 \\)", "3z*4y", "cos(x + y * 5)"],
+			answers: ["` x^2 + 2.5y^2 -y * exp(1-(x^2+y^2))* 0.3 `", "` 3y `", "` 3z*4y `", "` cos(x + y * 5) `"],
+            alreadyAsked: false
+        },
+        {
+            answers: ["` 1 - (x*x+y*y) `", "` cos(x+y) `", "` 1/ln(x*y) `", "` 2x+5y `"],
+            alreadyAsked: false
+        },
+        {
+            answers: ["` sin(x+y)*cos(y+x) `", "` cos(x+y)*sin(x*y) `", "` sin(x*y)*cos(y*x) `", "` tan(x+y) `"],
+            alreadyAsked: false
+        },
+        {
+            answers: ["` ln(1/x)*y `", "` cos(x+y)*sin(x*y) `", "` sin(x*y)*cos(y*x) `", "` tan(x+y) `"],
+            alreadyAsked: false
         }
     ];
